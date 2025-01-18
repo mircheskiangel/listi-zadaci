54892de67eef008f824fbfd2f428d75e64829c87
mircheskiangel <angel.mirceski57@gmail.com>
2025-01-18 11:43:42 +0100

    Initial commit

---
+++ src/APS_Ispitni/APSJan_2024.java
@@ -0,0 +1,15 @@
+//package APS_Ispitni;
+//
+//public class APSJan_2024 {
+//    DLLNode<String> node = list.getFirst();
+//    DLLNode<String> brisi_jazol = list.getFirst();
+//    int dolzina = 0;
+//    while(node!=null){
+//        if(node.element.length() >= dolzina){
+//            dolzina = node.element.length();
+//            brisi_jazol = node;
+//        }
+//        node = node.succ;
+//    }
+//    list.delete(brisi_jazol);
+//}
